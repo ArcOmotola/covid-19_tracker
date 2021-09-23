e68f6b5ec1b750c5a8ca09e4ee2592ab8f7203bf
ArcOmotola <ogunsola40@gmail.com>
2021-09-23 15:33:54 +0100

    hardcoded top card components and data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select } from '@material-ui/core';
+import InfoBox from './InfoBox';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  //https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const getCountriesData = async() => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+
+        setCountries(countries);
+        
+      })
+    }
+    console.log(countries)
+    getCountriesData();
+
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    console.log("yoooooo>>>>>", countryCode);
+
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Nigeria</MenuItem>
-          <MenuItem value="worldwide">Ghana</MenuItem>
-          <MenuItem value="worldwide">Russia</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide"> Worldwide </MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{ country.name }</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={683} total={200}/>
+        <InfoBox title="Recovered" cases={930} total={5670}/>
+        <InfoBox title="Deaths" cases={100} total={9200}/>
+      </div>
     </div>
   );
 }
